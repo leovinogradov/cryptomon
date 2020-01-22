44b624c3b21e5fb3f09c1b4748f765b2b4346052
Guy <gdarel@uci.edu>
2020-01-21 17:50:43 -0800

    added touch and button support

--- frontend/Test3.py
+++ frontend/Test3.py
@@ -1,4 +1,7 @@
 import pygame
+import os
+from time import sleep
+import RPi.GPIO as GPIO
 from option import Option
 from option import OptionWheel
 
@@ -6,9 +9,22 @@ class PygameView(object):
 
 
     def __init__(self, width=480, height=320, fps=30):
+        """Initialize GPIO pins
+        """
+        self.button_map = (17,27,22)
+        GPIO.setmode(GPIO.BCM)
+        for k in self.button_map:
+            GPIO.setup(k, GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
+        os.putenv('SDL_FBDEV','/dev/fb1')
+
+        GPIO.add_event_detect(self.button_map[0], GPIO.FALLING, callback=self.right_button,bouncetime=200)
+        GPIO.add_event_detect(self.button_map[1], GPIO.FALLING, callback=self.down_button,bouncetime=200)
+        GPIO.add_event_detect(self.button_map[2], GPIO.FALLING, callback=self.left_button,bouncetime=200)
+
         """Initialize pygame, window, background, font,...
         """
         pygame.init()
+        pygame.mouse.set_visible(False)
         pygame.display.set_caption("Press ESC to quit")
         self.width = width
         self.height = height
@@ -32,11 +48,20 @@ class PygameView(object):
                     if event.key == pygame.K_ESCAPE:
                         running = False
                     if event.key == pygame.K_RIGHT:
-                        self.opWheel.scroll_right()
+                        self.right_button(self.button_map[0])
                     if event.key == pygame.K_DOWN:
-                        self.opWheel.select()
+                        self.down_button(self.button_map[1])
                     if event.key == pygame.K_LEFT:
-                        self.opWheel.scroll_left()
+                        self.left_button(self.button_map[2])
+                elif event.type == pygame.MOUSEBUTTONUP:
+                    x, y = pygame.mouse.get_pos()
+                    if x > self.width*2/3:
+                        self.right_button(self.button_map[0])
+                    elif x > self.width/3:
+                        self.down_button(self.button_map[0])
+                    else:
+                        self.left_button(self.button_map[0])
+
             self.opWheel.draw_op_wheel()
             pygame.display.flip()
             self.screen.blit(self.background, (0, 0))
@@ -47,6 +72,13 @@ class PygameView(object):
         self.background = pygame.image.load("Stage.jpg")
         self.opWheel = OptionWheel(self)
         
+    def right_button(self,callback_type):
+        self.opWheel.scroll_right()
+    def down_button(self,callback_type):
+        self.opWheel.select()
+    def left_button(self,callback_type):
+        self.opWheel.scroll_left()
+    
     def draw_FPS(self):
         milliseconds = self.clock.tick(self.fps)
         self.playtime += milliseconds / 1000.0
@@ -62,7 +94,8 @@ class PygameView(object):
         surface = self.font.render(text, True, (0, 255, 0))
         # // makes integer division in python3
         self.screen.blit(surface, ((self.width - fw) // 2, (self.height - fh) // 2))
-
+    def deletethis(self):
+        print("deletethis")
 ####
 
 if __name__ == '__main__':
