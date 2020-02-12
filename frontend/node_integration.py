fb886775d5e7f270661e19104eaa082af9a1b12a
Leo Vinogradov <leovinogradov@gmail.com>
2020-02-12 15:32:01 -0800

    added more functions to node_integration

--- frontend/node_integration.py
+++ frontend/node_integration.py
@@ -12,6 +12,124 @@ def createmon(account):
         print(e)
         return -1;
 
+def deletemon(account, index):
+    try:
+        c = ["node", "integration.js", "deletemon"]
+        c.append("--account="+account)
+        c.append("--index="+index)
+        return subprocess.check_output(c)
+    except Exception as e:
+        print(e)
+        return -1;
+
+# List Cryptomon for sale
+# asset = the price of the cryptomon in the format [asset type]
+# delay = duration of sale
+# index = index of Cryptomon
+def listmon(account: string, asset: string, delay: number, index: number):
+    try:
+        c = ["node", "integration.js", "listmon"]
+        c.append("--account="+account)
+        c.append("--asset="+asset)
+        c.append("--delay="+delay)
+        c.append("--index="+index)
+        return subprocess.check_output(c)
+    except Exception as e:
+        print(e)
+        return -1;
+
+# Remove cryptomon from trade table
+def delistmon(account: string, index: number):
+    try:
+        c = ["node", "integration.js", "delistmon"]
+        c.append("--account="+account)
+        c.append("--index="+index)
+        return subprocess.check_output(c)
+    except Exception as e:
+        print(e)
+        return -1;
+
+def purchasemon(account: string, index: number):
+    try:
+        c = ["node", "integration.js", "purchasemon"]
+        c.append("--account="+account)
+        c.append("--index="+index)
+        return subprocess.check_output(c)
+    except Exception as e:
+        print(e)
+        return -1;
+
+def accepttrade(account: string, index: number):
+    try:
+        c = ["node", "integration.js", "accepttrade"]
+        c.append("--account="+account)
+        c.append("--index="+index)
+        return subprocess.check_output(c)
+    except Exception as e:
+        print(e)
+        return -1;
+
+def inittrade(account1: string, account2: string, price: string, swap: boolean, duration: number, c1: number, c2: number):
+    try:
+        c = ["node", "integration.js", "inittrade"]
+        c.append("--account1="+account1)
+        c.append("--account2="+account2)
+        c.append("--price="+price)
+        c.append("--swap="+swap)
+        c.append("--duration="+duration)
+        c.append("--c1="+c1)
+        c.append("--c2="+c2)
+        return subprocess.check_output(c)
+    except Exception as e:
+        print(e)
+        return -1;
+
+def canceltrade(account: string, index: number):
+    try:
+        c = ["node", "integration.js", "canceltrade"]
+        c.append("--account="+account)
+        c.append("--index="+index)
+        return subprocess.check_output(c)
+    except Exception as e:
+        print(e)
+        return -1;
+
+
+# set name of cryptomon
+def setname(account: string, name: string, index: number):
+    try:
+        c = ["node", "integration.js", "setname"]
+        c.append("--account="+account)
+        c.append("--name="+name)
+        c.append("--index="+index)
+        return subprocess.check_output(c)
+    except Exception as e:
+        print(e)
+        return -1;
+
+# apply item in player inventory to cryptomon
+def itemapply(account: string, item: number, index: number):
+    try:
+        c = ["node", "integration.js", "itemapply"]
+        c.append("--account="+account)
+        c.append("--item="+item)
+        c.append("--index="+index)
+        return subprocess.check_output(c)
+    except Exception as e:
+        print(e)
+        return -1;
+
+# create player for account
+# name = name of player
+def upsertplayer(account: string, name: string):
+    try:
+        c = ["node", "integration.js", "upsertplayer"]
+        c.append("--account="+account)
+        c.append("--s="+name)
+        return subprocess.check_output(c)
+    except Exception as e:
+        print(e)
+        return -1;
 
 def test():
     try:
